0c5e61cda80fadfe4563e4e0d07defb905efd911
MANOJ_SM <manumedleri@gmail.com>
2025-02-10 20:38:07 +0530

    `Added notes on creating a React app and JSX usage`

--- index.jsx
+++ index.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const header1 = <h1 id="heading"> this is react</h1>
-const rootHeader = ReactDOM.createRoot(document.getElementById("root"));
-rootHeader.render(header1);
+
+// 1. using  normal react 
+
+// const header1 = <h1 id="heading"> this is react</h1>
+// const rootHeader = ReactDOM.createRoot(document.getElementById("root"));
+// rootHeader.render(header1);
+
+// 2. using jsx for single ele creation
+
+// const jsxHeader = <h1>hi i am manoj </h1>
+// const rootHeader = ReactDOM.createRoot(document.getElementById("root"));
+// rootHeader.render(jsxHeader);
+
+// 3. using jsx for multiple ele creation
+
+// const jsxHeader = (
+
+//     <>
+//     <h1 id='heading' className='header'>manu lets not stop</h1>
+//     <div>
+//         <h2>this is what it is </h2>
+//     </div>
+//     </>
+// );
+
+// const rootHeader = ReactDOM.createRoot(document.getElementById("root"));
+// rootHeader.render(jsxHeader);
